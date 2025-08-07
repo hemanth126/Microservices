b7b0325c7cb5aeafd54639eae7b08d26002720a3
hemanth126 <121964384+hemanth126@users.noreply.github.com>
2025-08-07 17:47:05 +0530

    Update KafkaMessageListener.java

--- consumer-service/src/main/java/com/dis/consumer_service/KafkaMessageListener.java
+++ consumer-service/src/main/java/com/dis/consumer_service/KafkaMessageListener.java
@@ -10,5 +10,7 @@ public class KafkaMessageListener {
     @KafkaListener(topics = "quickstart-events", groupId = "consumer-group-1")
     public void listen(String message) {
         System.out.println("✅ Consumed: " + message);
+
+//hello hi namaste 
     }
 }
