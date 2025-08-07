521c9fb8f5b6142982e335094e869e8a5ce3da59
hemanth126 <hemanthsoldier7095@gmail.com>
2025-08-07 17:42:26 +0530

    Added basic user service

---
+++ consumer-service/src/main/java/com/dis/consumer_service/KafkaMessageListener.java
@@ -0,0 +1,14 @@
+package com.dis.consumer_service;
+
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaMessageListener {
+
+    @KafkaListener(topics = "quickstart-events", groupId = "consumer-group-1")
+    public void listen(String message) {
+        System.out.println("✅ Consumed: " + message);
+    }
+}
---
+++ producer-service/src/main/java/com/dis/producer_service/KafkaProducerController.java
@@ -0,0 +1,20 @@
+package com.dis.producer_service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/messages")
+public class KafkaProducerController {
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    private static final String TOPIC = "quickstart-events";
+
+    @PostMapping
+    public String publish(@RequestParam("message") String message) {
+        kafkaTemplate.send(TOPIC, message);
+        return "Message sent: " + message;
+    }
+}
